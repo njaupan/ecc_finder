b08b0784ca9aef8b29e4c1421718e6e6fff78e1b
Ivy <njaupanpan@gmail.com>
2021-08-25 12:32:40 +0200

    Update asm-sr.py

--- asm-sr.py
+++ asm-sr.py
@@ -56,25 +56,31 @@ def run_fastp(file_prefix,output_path, query1_file,query2_file,num_threads, over
         fastp_cmd = "fastp --thread "+ str(num_threads) +" -i " + query1_file+ " -I "+ query2_file +" -o " + query1_file_out+ " -O "+ query2_file_out+" -h "+ html_file_out
         subprocess.call(fastp_cmd, shell=True) 
 
-def run_Unicycler(file_prefix,query_file,output_path, num_threads,num_len,overwrite_files):
+def run_asm(file_prefix,output_path, num_threads,num_len,overwrite_files):
     """ assembly short read. """
-    if os.path.isfile(output_path +"contig.fasta"):
+    if os.path.isfile(output_path +file_prefix+".fasta"):
         if not overwrite_files:
-            log("INFO", "Retaining pre-existing file: " + output_path +"contig.fasta")
+            log("INFO", "Retaining pre-existing file: " + output_path +file_prefix+".fasta")
         else:
-            log("INFO", "Overwriting pre-existing file: " +output_path +"contig.fasta")
-            Spades_params = " -t "+ str(num_threads) +" --min_fasta_length " + str(num_len) 
-            Spades_cmd = "unicycler -1 "+ output_path +file_prefix+".R1.fastq.gz"+ " -2 "+ output_path +file_prefix+".R2.fastq.gz" +Spades_params 
-            subprocess.call(Spades_cmd, shell=True) 
+            log("INFO", "Overwriting pre-existing file: " +output_path +file_prefix+".fasta")
+            asm_params = " -t "+ str(num_threads) +" --min_fasta_length " + str(num_len) +" --keep 0 -o " + output_path
+            asm_cmd = "unicycler -1 "+ output_path +file_prefix+".R1.fastq.gz"+ " -2 "+ output_path +file_prefix+".R2.fastq.gz" +asm_params
+            subprocess.call(asm_cmd, shell=True) 
+            cmd = "mv " +output_path +"assembly.fasta " + output_path +file_prefix+"assembly.fasta && mv " +output_path +"assembly.gfa " + output_path +file_prefix+"assembly.gfa"
+            subprocess.call(cmd, shell=True)
     else:  
-        Spades_params = " -t "+ str(num_threads) +" --min_fasta_length " + str(num_len) 
-        Spades_cmd = "unicycler -1 "+ output_path +file_prefix+".R1.fastq.gz"+ " -2 "+ output_path +file_prefix+".R2.fastq.gz" +Spades_params 
-        subprocess.call(Spades_cmd, shell=True) 
+        asm_params = " -t "+ str(num_threads) +" --min_fasta_length " + str(num_len) +" --keep 0 -o " + output_path
+        asm_cmd = "unicycler -1 "+ output_path +file_prefix+".R1.fastq.gz"+ " -2 "+ output_path +file_prefix+".R2.fastq.gz" +asm_params 
+        subprocess.call(asm_cmd, shell=True) 
+        cmd = "mv " +output_path +"assembly.fasta " + output_path +file_prefix+".fasta && mv " +output_path +"assembly.gfa " + output_path +file_prefix+".gfa"
+        subprocess.call(cmd, shell=True)
+
 
 def main():
     description = "A tool to detect eccDNA loci using ONT sequencing"
     parser = argparse.ArgumentParser(description=description, usage="ecc_finder.py asm-sr <query.fq1> <query.fq2> (option)")
-    parser.add_argument("query", metavar="<query.fq>", nargs='?', default="", type=str, help="query fastq file (uncompressed or bgzipped)")
+    parser.add_argument("query1", metavar="<query1.fq>", nargs='?', default="", type=str, help="query forward fastq file (uncompressed or bgzipped)")
+    parser.add_argument("query2", metavar="<query2.fq>", nargs='?', default="", type=str, help="query reverse fastq file (uncompressed or bgzipped)")
 
     asm_options = parser.add_argument_group("asm options")   
     asm_options.add_argument('-t', metavar="INT",type=int, default=100,
@@ -82,26 +88,22 @@ def main():
     asm_options.add_argument('-l', metavar="INT",type=int, default=get_default_thread(),help='minimum fasta length of assembly[100]')
 
     out_options = parser.add_argument_group("output options")
-    out_options.add_argument("-o", metavar="PATH", type=str, default="eccFinder_output", help="output directory [./eccFinder_output]")
+    out_options.add_argument("-o", metavar="PATH", type=str, default="eccFinder_asm_output", help="output directory [./eccFinder_asm_output]")
     out_options.add_argument("-w", action='store_true', default=False, help="overwrite intermediate files")
-    out_options.add_argument("-x", type=str, default="ecc.ont", help="add prefix to output [ecc.ont]")
+    out_options.add_argument("-x", type=str, default="ecc.asm.sr", help="add prefix to output [ecc.asm.sr]")
     out_options.add_argument("--debug", action='store_true', default=False, help=argparse.SUPPRESS)
 
     args = parser.parse_args()
 
-    if not args.query:
+    if not args.query1 and not args.query2:
         parser.print_help()
-        sys.exit("\n** The query files are required **")
+        sys.exit("\n** The paired end query files are required **")
 
     log("VERSION", "ecc_finder " + get_eccFinder_version())
-    log("CMD", "ecc_finder.py asm-sr " + " ".join(sys.argv[1:]))
+    log("CMD", "python ecc_finder.py asm-sr " + " ".join(sys.argv[1:]))
 
-    query_file = os.path.abspath(args.query)
     query1_file = os.path.abspath(args.query1)
     query2_file = os.path.abspath(args.query2)
-    
-    if not os.path.isfile(query_file):
-        raise FileNotFoundError("Could not find file: %s" % query_file)
     
     num_threads = args.t
     num_len = args.l
@@ -120,9 +122,9 @@ def main():
     run_fastp(file_prefix,output_path, query1_file,query2_file,num_threads, overwrite_files)
 
     #Consensus sequences of each tandem repeat for a long read
-    log("INFO", "Run Unicycler assembmy at a wide range of k-mer sizes, evaluating the graph at each one")
+    log("INFO", "Run assembmy mode at a wide range of k-mer sizes, evaluating the graph at each one")
     log("INFO", "Which will be the most time-comsuming")
-    run_Unicycler(file_prefix,query_file,output_path, num_threads,num_len,overwrite_files)
+    run_asm(file_prefix,output_path, num_threads,num_len,overwrite_files)
 
     log("INFO", "Finished running ecc_finder")
     log("INFO", "Goodbye, have a nice day!")
